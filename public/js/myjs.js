e09d54ac9db20e217300ea15dcfcc6ca8a0e3264
maintth1807076 <maintth1807076@fpt.edu.vn>
2019-05-23 14:15:48 +0700

    xong register

---
+++ public/js/myjs.js
@@ -0,0 +1,78 @@
+$('#login-form').validate({
+    rules: {
+        'email': {
+            required: true,
+            email: true
+        },
+        'password': {
+            required: true,
+            minlength: 6,
+        }
+    },
+    messages: {
+        'email': {
+            required: 'Please fill email.',
+            email: 'Please fill email follow format @domain.com.'
+        },
+        'password': {
+            required: 'Please fill password.',
+            minlength: 'Password is too short.',
+        }
+    }
+});
+$('#register-form').validate({
+    rules: {
+        'firstname': {
+            required: true,
+            firstname: true
+        },
+        'lastname': {
+            required: true,
+            lastname: true
+        },
+        'address': {
+            required: true,
+            address: true
+        },
+        'email': {
+            required: true,
+            email: true
+        },
+        'numbercmnd': {
+            required: true,
+            email: true
+        },
+        'password': {
+            required: true,
+            minlength: 6,
+        },
+        'password_confirmation': {
+            equalTo: '[name="password"]'
+        }
+    },
+    messages: {
+        'firstname': {
+            required: 'Please fill firstname.',
+        },
+        'lastname': {
+            required: 'Please fill lastname.',
+        },
+        'address': {
+            required: 'Please fill address.',
+        },
+        'email': {
+            required: 'Please fill email.',
+            email: 'Please fill email follow format @domain.com.'
+        },
+        'numbercmnd': {
+            required: 'Please fill number identity.',
+        },
+        'password': {
+            required: 'Please fill password.',
+            minlength: 'Password is too short.',
+        },
+        'password_confirmation': {
+            equalTo: 'Password not same.'
+        }
+    }
+});
--- routers/other.router.js
+++ routers/other.router.js
@@ -11,5 +11,11 @@ router.get('/contact',function (req,res) {
 router.get('/about',function (req,res) {
     res.render('client/about', { 'user': req.user })
 })
+router.get('/departments',function (req,res) {
+    res.render('client/departments', { 'user': req.user })
+})
+router.get('/doctors',function (req,res) {
+    res.render('client/doctors', { 'user': req.user })
+})
 
 module.exports = router; 
